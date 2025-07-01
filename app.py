5d403101658145746f234060d21fbf806069aa28
robsoncampelo <38872562+robsoncampelo@users.noreply.github.com>
2025-07-01 19:44:41 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -543,7 +543,11 @@ Este **TERMO** terá vigência conforme descrito na tabela abaixo, podendo ser r
     )
 
 
-demo.launch()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
+    demo.queue().launch(server_name="0.0.0.0", server_port=port)
 
 
 # In[ ]:
